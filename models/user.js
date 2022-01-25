5c5fefd578bc1235bf22d2d9a5d7d19ab63ee5e4
James Spencer <jameskcs1254@gmail.com>
2022-01-25 14:20:28 +0000

    Change membership status field type

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ const UserSchema = new Schema(
     lastName: {type: String, required: true, maxLength: 100},
     username: {type: String, required: true, maxLength: 100},
     password: {type: String, required: true, minLength:6, maxLength: 100},
-    membershipStatus: {type: String, required: true, maxLength: 100}
+    membershipStatus: {type: Boolean, required: true}
   }
 );
 
