10adacc5869b3753851484488b5758cd8eb4133b
James Spencer <jameskcs1254@gmail.com>
2022-01-25 12:29:04 +0000

    fix filepath

--- routes/signUp.js
+++ routes/signUp.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const signUpController = require('/controllers/signUpController')
+const signUpController = require('../controllers/signUpController')
 
 router.get('/', signUpController.signUpGet);
 router.post('/', signUpController.signUpPost);
