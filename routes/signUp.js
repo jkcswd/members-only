79ad7a097555a5c282ffe3e5e51dc27030d794b5
James Spencer <jameskcs1254@gmail.com>
2022-01-25 11:59:19 +0000

    Import controller functions and call them.

--- routes/signUp.js
+++ routes/signUp.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const signUpController = require('/controllers/signUpController')
+
+router.get('/', signUpController.signUpGet);
+router.post('/', signUpController.signUpPost);
 
 module.exports = router;
