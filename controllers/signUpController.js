65bd966a36a40fb679e9082691b9ca56e6e987b7
James Spencer <jameskcs1254@gmail.com>
2022-01-25 12:46:18 +0000

    implement post controller for signup

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -4,6 +4,18 @@ exports.signUpGet = (req, res, next) => {
   res.render('signUpForm');
 };
 
-exports.signUpPost = (req, res, next) =>{ 
-  
+exports.signUpPost = (req, res, next) => { 
+  const user = new User(
+    {
+      firstName: req.body.first_name,
+      lastName: req.body.last_name,
+      username: req.body.username,
+      password: req.body.password,
+      membershipStatus: 'member'
+    }
+  ).save(err => {
+    if (err) { return next(err); }
+
+    res.redirect('/');
+  })
 };
