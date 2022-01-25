07263333252c3781e6fec0de56dc2c4c808d95af
James Spencer <jameskcs1254@gmail.com>
2022-01-25 14:20:51 +0000

    Add sanitisation

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,26 +1,42 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs')
+const { body,validationResult } = require('express-validator');
 
 exports.signUpGet = (req, res, next) => { 
   res.render('signUpForm');
 };
 
-exports.signUpPost = (req, res, next) => { 
-  bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
-    if (err) { return next(err); }
+exports.signUpPost = [
+  // TODO: add more validation
+  body('first_name', 'Empty first name').trim().isLength({ min: 1 }).escape(),
+  body('last_name', 'Empty last name').trim().isLength({ min: 1 }).escape(),
+  body('username', 'Empty username').trim().isLength({ min: 1 }).escape(),
+  body('password', 'password must be 6 characters').trim().isLength({ min: 6 }).escape(),
+  body('confirm_password', 'passwords must match').trim().custom((value, { req }) => value === req.body.password).escape(),
 
-    const user = new User(
-      {
-        firstName: req.body.first_name,
-        lastName: req.body.last_name,
-        username: req.body.username,
-        password: hashedPassword,
-        membershipStatus: 'member'
-      }
-    ).save(err => {
-      if (err) { return next(err); }
+  (req, res, next) => { 
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render('signUpForm', { errors: errors.array() });
+    }else {
+      bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        if (err) { return next(err); }
   
-      res.redirect('/');
-    })
-  })
-};
+        const user = new User(
+          {
+            firstName: req.body.first_name,
+            lastName: req.body.last_name,
+            username: req.body.username,
+            password: hashedPassword,
+            membershipStatus: false
+          }
+        ).save(err => {
+          if (err) { return next(err); }
+      
+          res.redirect('/');
+        })
+      })
+    } 
+  }
+]
