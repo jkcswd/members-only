fe2ed0998c207c136e4589da268f4b606f382683
James Spencer <jameskcs1254@gmail.com>
2022-01-25 12:57:07 +0000

    hash password with bcrypt

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,21 +1,26 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs')
 
 exports.signUpGet = (req, res, next) => { 
   res.render('signUpForm');
 };
 
 exports.signUpPost = (req, res, next) => { 
-  const user = new User(
-    {
-      firstName: req.body.first_name,
-      lastName: req.body.last_name,
-      username: req.body.username,
-      password: req.body.password,
-      membershipStatus: 'member'
-    }
-  ).save(err => {
+  bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
     if (err) { return next(err); }
 
-    res.redirect('/');
+    const user = new User(
+      {
+        firstName: req.body.first_name,
+        lastName: req.body.last_name,
+        username: req.body.username,
+        password: hashedPassword,
+        membershipStatus: 'member'
+      }
+    ).save(err => {
+      if (err) { return next(err); }
+  
+      res.redirect('/');
+    })
   })
 };
