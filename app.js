71a2ced958ead34e61c6de69f6b98fce61342db1
James Spencer <jameskcs1254@gmail.com>
2022-01-25 11:56:37 +0000

    Add sign up route. Fix mongo string variable

--- app.js
+++ app.js
@@ -6,13 +6,11 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const signUpRouter = require('./routes/signUp');
 
 //MongoDB
-
-const mongoDb = process.env.MONGOSTRING;
+const mongoDb = process.env.MONGO_STRING;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error'));
@@ -30,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/sign-up', signUpRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
