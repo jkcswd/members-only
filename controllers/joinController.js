037d35ef10995caf7cf1d033d3a130967dc8ce5a
James Spencer <jameskcs1254@gmail.com>
2022-01-26 10:19:09 +0000

    Implement post controller for join page.

--- controllers/joinController.js
+++ controllers/joinController.js
@@ -1,11 +1,19 @@
 const User = require('../models/user');
 const { body, validationResult } = require('express-validator');
+const password = 'PASSWORD'
 
 exports.joinGet = (req, res, next) => { 
   res.render('join');
 };
 
 exports.joinPost = [
-  body('password', 'Empty Password').trim().isLength({ min: 1 }).escape(),
+  body('password', 'Password not correct').trim().custom((value, password) => value === password).escape(),
 
+  (req, res, next) => { 
+    User.findByIdAndUpdate(req.user.id, { membershipStatus: true }, {}, (err, doc) => {
+      if(err) { return next(err); }
+
+      res.send('You have joined the club')
+    });
+  }
 ]
