f20ff3ede272388d55b83d448d8ecfaadd0c2c70
James Spencer <jameskcs1254@gmail.com>
2022-01-26 10:18:33 +0000

    import user module

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require('bcryptjs');
+const User = require('../models/user')
 
 module.exports = (passport) => {
   passport.serializeUser((user, done) => {
