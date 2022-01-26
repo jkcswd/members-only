75a44380c6611ce2292f25dfecda7b3b5278f1fa
James Spencer <jameskcs1254@gmail.com>
2022-01-26 12:05:14 +0000

    Extract messages from DB for rendering

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 const passport = require('passport');
+const Message = require('../models/message');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index');
+  Message.find().populate('user').exec((err, messages) => {
+    if(err) { return next(err); }
+
+    res.render('index', { messages });
+  })  
 });
 
 router.post('/login', passport.authenticate('local', { successRedirect: "/", failureRedirect: "/" }));
