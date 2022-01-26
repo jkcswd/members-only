2b1ba51e0df2d2e20260375028208d9cfd5ab693
James Spencer <jameskcs1254@gmail.com>
2022-01-26 10:34:38 +0000

    Add login post route. Edit index route.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const passport = require('passport');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.post('/login', passport.authenticate('local', { successRedirect: "/", failureRedirect: "/" }));
 
 module.exports = router;
