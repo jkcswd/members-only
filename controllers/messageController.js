159da02aa6dc09413f74d44611c856a81ce9a990
James Spencer <jameskcs1254@gmail.com>
2022-01-26 11:43:18 +0000

    Implement get and post controller functions.

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,10 +1,34 @@
-const User = require("../models/user")
-const Message = require('../models/message')
+const User = require("../models/user");
+const Message = require('../models/message');
+const { body,validationResult } = require('express-validator');
 
 exports.messageGet = (req, res, next) => {
-
+  res.render('messageForm');
 }
 
 exports.messagePost = [
+  body('title', 'Empty title').trim().isLength({ min: 1 }).escape(),
+  body('message', 'Empty message').trim().isLength({ min: 1 }).escape(),
 
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render('messageForm', { errors: errors.array() });
+    }else {
+      const message = new Message(
+        {
+          title: req.body.title,
+          message: req.body.message,
+          date: new Date(),
+          user: req.user.id,
+        }
+      ).save(err => {
+        if (err) { return next(err); }
+    
+        res.redirect('/');
+      })
+    } 
+
+  }
 ]
